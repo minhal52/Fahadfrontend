87c6d159586dbaf9d424d049395e0c6207c7661e
minhal52 <minhalhanief@gmail.com>
2025-03-06 18:40:49 +0530

    added new code

--- src/pages/EquipmentRental.jsx
+++ src/pages/EquipmentRental.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const EquipmentRentals = () => {
-  const navigate = useNavigate(); // React Router navigation hook
+  const navigate = useNavigate();
 
   const equipmentList = [
     {
